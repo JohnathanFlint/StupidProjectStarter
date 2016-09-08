7cce1c581323966eadc425fd7509f9a575671e29
JohnathanFlint <nicholas.white142@gmail.com>
2016-09-08 08:39:49 -0600

    Added a constructor that is my name

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -2,8 +2,14 @@ package stupid.controller;
 
 public class StupidController 
 {
+	private String myName;
+	public StupidController() //constructor = public name of class
+	{
+		myName = "Spatula";
+	}
 	public void start()
 	{
 		System.out.println("this program is as stupid as before");
+		System.out.println(myName);
 	}
 }
